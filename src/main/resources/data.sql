977f0f1f5e2e4baa54bfecf058403828b555359d
mauriarroqui <mauriarroqui@gmail.com>
2018-09-08 10:09:38 -0300

    SQL Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user
+values(1,'Tandil','Tandil',1111,'Buenos Aires','Una Calle','7000','Cristian','Mateo','cmateos2006@gmail.com','cmateo');
+
+insert into user_recycling 
+values(1,1,2,'2018-09-13',3,4,5,1)
